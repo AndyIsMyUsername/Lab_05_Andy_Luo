18222747897435f935b508c5c124dbadc587b0b6
luoan <luoan@10.16.173.233>
2025-10-06 10:43:47 -0400

    Task 2

---
+++ src/RestaurantBill.java
@@ -0,0 +1,119 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author luoan
+ */
+public class RestaurantBill extends Application{
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+//        private double subtotal = 0.0;
+//        private double total = 0.0;
+               
+                
+        //add whole food item for user to choose
+        ComboBox<String> beverageComboBox = new ComboBox<>();
+        beverageComboBox.getItems().addAll("Coffee", "Tea", "Soft Drink", "Water", "Milk", " Juice");
+        ComboBox<String> appetizerComboBox = new ComboBox<>();
+        appetizerComboBox.getItems().addAll("Soup", "Salad", "Spring Roll", "Garlic Bread", "Chips and Salsa");
+        ComboBox<String> mainCourseComboBox = new ComboBox<>();
+        mainCourseComboBox.getItems().addAll("Steak", "Grilled Chiken", "Chicken Alfredo" , "Turkey Club", "Shrimp Scampi", " Pasta" ,"Fish and Chips");
+        ComboBox<String> dessertComboBox = new ComboBox<>();
+        dessertComboBox.getItems().addAll("Apple Pie", "Carrot Cake", " Mud Pie", "Pudding", "Apple Crisp");
+        
+        //Map
+        Map<String, Double> items = new HashMap<>();
+        //beverages
+        items.put("Coffee", 2.50);
+        items.put("Tea", 2.00);
+        items.put("Soft Drink", 1.75);
+        items.put("Water", 2.95);
+        items.put("Milk", 1.50);
+        items.put("Juice", 2.50);
+        //appetizer
+        items.put("Soup", 4.50);
+        items.put("Salad", 3.75);
+        items.put("Spring Rolls", 5.25);
+        items.put("Garlic Bread", 3.00);
+        items.put("Chips and Salsa", 6.95);
+        
+        //main
+        items.put("Steak", 15.00);
+        items.put("Grilled Chiken", 13.50);
+        items.put("Chicken Alfredo", 13.95);
+        items.put("Turkey Club", 11.90);
+        items.put("Shrimp Scampi", 18.99);
+        items.put("Pasta", 11.75);
+        items.put("Fish and Chips", 12.25);
+
+        //dessert
+        items.put("Apple Pie", 5.95);
+        items.put("Carrot Cake", 4.50);
+        items.put("Mud Pie", 4.75);
+        items.put("Pudding", 3.25);
+        items.put("Apple Crisp", 5.98);
+        
+        
+        //Labels
+        Label bevLabel = new Label("Beverages: ");
+        Label appetLabel = new Label("Appetizer: ");
+        Label mainLabel = new Label("Main Dishes: ");
+        Label dessertLabel = new Label("Deesrts: ");
+        
+        //slider for tip%
+        Slider tipSlider = new Slider(0.0,20.0,10.0);
+        tipSlider.setShowTickLabels(true);
+        tipSlider.setShowTickMarks(true);
+        tipSlider.setMajorTickUnit(5);
+        tipSlider.setOrientation(Orientation.HORIZONTAL);
+        
+        Button clearButton = new Button("Clear Button");
+                
+        
+        GridPane root = new GridPane();
+        //add labels
+        root.add(bevLabel, 0, 0);
+        root.add(appetLabel, 0, 1);
+        root.add(mainLabel, 0, 2);
+        root.add(dessertLabel, 0, 3);
+        //add Combo Box
+        root.add(beverageComboBox, 1, 0);
+        root.add(appetizerComboBox, 1, 1);
+        root.add(mainCourseComboBox, 1, 2);
+        root.add(dessertComboBox, 1, 3);
+        
+        root.add(tipSlider, 0, 4);
+        
+        root.setHgap(10);
+        root.setVgap(10);
+        
+        
+        
+        Scene scene = new Scene(root,500,500);
+        stage.setScene(scene);
+        stage.setTitle("Restaurant bill");
+        stage.show();
+    }
+}
